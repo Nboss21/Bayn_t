80e8527079ebcbf136c832c365a24489d3dd3e85
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-09-03 11:54:12 +0300

    refactor: replace hardcoded image imports in GalleryGrid with dynamic data from galleryData for improved maintainability and flexibility

--- frontend/src/data/home/galleryData.js
+++ frontend/src/data/home/galleryData.js
@@ -4,12 +4,14 @@ import img3 from "../../assets/womans/woman 3.png";
 import img4 from "../../assets/womans/woman 4.png";
 import img5 from "../../assets/womans/woman 5.jpg";
 import img6 from "../../assets/womans/woman 6.png";
+import img7 from "../../assets/womans/woman 7.jpg";
 
 export const galleryImages = [
-  { id: 1, src: img1, alt: "Gallery 1" },
-  { id: 2, src: img2, alt: "Gallery 2" },
-  { id: 3, src: img3, alt: "Gallery 3" },
-  { id: 4, src: img4, alt: "Gallery 4" },
-  { id: 5, src: img5, alt: "Gallery 5" },
-  { id: 6, src: img6, alt: "Gallery 6" },
+  { id: 1, src: img1, alt: "Woman 1", colSpan: 2, height: "tall" },
+  { id: 2, src: img2, alt: "Woman 2", colSpan: 1, height: "tall" },
+  { id: 3, src: img3, alt: "Woman 3", colSpan: 1, height: "medium" },
+  { id: 4, src: img4, alt: "Woman 4", colSpan: 2, height: "medium" },
+  { id: 5, src: img5, alt: "Woman 5", colSpan: 1, height: "short" },
+  { id: 6, src: img6, alt: "Woman 6", colSpan: 1, height: "short" },
+  { id: 7, src: img7, alt: "Woman 7", colSpan: 1, height: "short" },
 ];
